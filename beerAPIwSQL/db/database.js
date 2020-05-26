392d438aae49065dcb887da17612c5d65caa3ec5
et <eric.thomson13@gmail.com>
2020-05-26 15:20:30 -0600

    Servies Added

--- beerAPIwSQL/db/database.js
+++ beerAPIwSQL/db/database.js
@@ -4,19 +4,19 @@ import { Client } from "https://deno.land/x/postgres/mod.ts";
 class Database {
   constructor() {
     this.connect();
-  };
+  }
 
-  async connect () {
+  async connect() {
     this.client = new Client({
       user: "postgres",
       database: "beers",
-      host: 'localhost',
-      password: 'postgres',
-      port: '5432',
+      host: "localhost",
+      password: "postgres",
+      port: "5432",
     });
 
     await this.client.connect();
   }
-};
+}
 
 export default new Database().client;
