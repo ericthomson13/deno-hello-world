a8916efc052dbc1d669f9086c136ebd9c7756b18
et <eric.thomson13@gmail.com>
2020-05-26 15:38:26 -0600

    Notes on BeerAPI Needs

--- beerAPIwSQL/app.js
+++ beerAPIwSQL/app.js
@@ -1,3 +1,11 @@
+/*
+
+This project is not connecting to the postgreSQL db and that is causing the server to error out.
+Need to troubleshoot this
+Also should upgrade modules to Typescript to minimize warnings
+
+*/
+
 import { Application } from "https://deno.land/x/oak/mod.ts";
 import router from "./routes.js";
 import _404 from "./controllers/404.js";
